d195239191b5ceb2d6bd5d1dc4bfc67b9778fed7
Mark Tan <mtcy_9012@hotmail.com>
2016-08-13 17:03:03 +0800

    lesson 2

--- App.jsx
+++ App.jsx
@@ -1,23 +1,41 @@
 import React from "react";
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      header: "this is the fucking header",
+      content: "this is the stupid content"
+    }
+  }
+
   render() {
     return (
-      <div>
-        <h1>{this.props.headerProp}</h1>
-        <h2>{this.props.contentProp}</h2>
+      <div class="wrapper">
+        <Header headerProps = {this.state.header} />
+        <Content contentProps = {this.state.content} />
       </div>
     );
   }
+
 }
 
 class Header extends React.Component {
   render() {
-    return (
-      <h1>Here is the fuckin header</h1>
+    return(
+      <h1>{this.props.headerProps}</h1>
     );
   }
 }
 
+class Content extends React.Component {
+  render() {
+    return(
+      <h3>{this.props.contentProps}</h3>
+    );
+  }
+}
 
 export default App;
